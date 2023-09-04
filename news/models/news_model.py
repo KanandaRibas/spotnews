dd5bdec2df34688e1c6369851c25bbdebfc13cc9
Kananda Ribas <kanandaribas@gmail.com>
2023-09-04 15:39:55 -0300

    fix: Alteração na propriedade 'created_at' na 'news_model'

--- news/models/news_model.py
+++ news/models/news_model.py
@@ -5,12 +5,13 @@ from news.models.validators import validate_title
 class News(models.Model):
     title = models.CharField(
         max_length=200, validators=[validate_title])
-    content = models.TextField(default="content")
+    content = models.TextField()
     author = models.ForeignKey(
         'Users',
         on_delete=models.CASCADE,
     )
-    created_at = models.DateField(auto_now=True)
+    created_at = models.DateField(
+        default="2023-08-08")
     image = models.ImageField(
         upload_to="img/", null=True, blank=True
         )
