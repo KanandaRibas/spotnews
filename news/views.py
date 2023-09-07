1d9ba37dce742292aae177259ed941c3b43b9f65
Kananda Ribas <kanandaribas@gmail.com>
2023-09-07 17:41:27 -0300

    feat: Adiciona template 'news_details.html' com urls e views para a página de detalhes da notícia

--- news/views.py
+++ news/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from news.models.news_model import News
 
 
 def index(request):
     context = {"news": News.objects.all()}
     return render(request, 'home.html', context)
+
+
+def news_details(request, news_id):
+    news = get_object_or_404(News, id=news_id)
+    context = {
+        'news': news,
+        'categories': news.categories.all()
+    }
+    return render(
+        request, 'news_details.html', context)
