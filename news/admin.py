aa6a6088f0f18989323cc2db333206a58cb8cb30
Kananda Ribas <kanandaribas@gmail.com>
2023-09-12 21:50:08 -0300

    feat: Adiciona a rota 'api/categories' com o DRF

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import category_model, user_model, news_model
 
-# Register your models here.
+
+admin.site.register(category_model.Categories)
+admin.site.register(user_model.Users)
+admin.site.register(news_model.News)
